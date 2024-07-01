afdd72e982494cf3707187151a8a0237643dbd04
ElyasYid <elyassayle27@gmail.com>
2024-07-01 16:28:38 +0000

    vbbb

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,8 @@
 /**
  * Retrieves students in a given location.
- * @param {{id:firstName:location:city}}}
+ * @param {{ id: Number,firstName: String,location: String
+ * }[]} students - The list of students.
+ * @param {String} city - The location.
  * @returns
  */
 export default function getStudentsByLocation(students, city) {
