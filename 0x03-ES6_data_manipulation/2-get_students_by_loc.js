0b289a65b53e3e78fbc70a02b574727eda163276
ElyasYid <elyassayle27@gmail.com>
2024-07-01 16:34:19 +0000

    ala

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -3,7 +3,7 @@
  * @param {{ id: Number,firstName: String,location: String
  * }[]} students - The list of students.
  * @param {String} city - The location.
- * @returns
+ * @returns {Array<{ id: number, firstName: string, location: string }>}
  */
 export default function getStudentsByLocation(students, city) {
   if (students instanceof Array) {
